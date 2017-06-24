d7721c2e96132609b8bf9a6ab37a5e26e1035fa5
lzzhaha <zlinai@connect.ust.hk>
2017-06-24 12:36:21 +0800

    finish preliminary implementation

--- flights.c
+++ flights.c
@@ -1,9 +1,3 @@
-/* 
- * CS61C Summer 2013
- * Name:
- * Login:
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,14 +6,23 @@
 
 struct flightSys {
     // Place the members you think are necessary for the flightSys struct here.
+	airport_t* airports;
 };
 
 struct airport {
     // Place the members you think are necessary for the airport struct here.
+	char* name;
+	flight_t* flights;
+	airport* next; 
 };
 
 struct flight {
     // Place the members you think are necessary for the flight struct here.
+	airport_t *destination;
+	time_HM departure_time;
+	time_HM arrival_time;
+	int cost;
+	flight* next;
 };
 
 /*
@@ -36,8 +39,14 @@ static void allocation_failed() {
    Returns a pointer to the system created.
  */
 flightSys_t* createSystem() {
-    // Replace this line with your code
-    return NULL;
+    //Create an empty system at first time
+
+	flightSys_t* s = (flightSys_t*)malloc(sizeof(flightSys_t));
+	if(s == NULL){
+		allocation_failed();
+	}
+	s->airports = NULL;
+	return s;
 }
 
 
@@ -46,8 +55,13 @@ flightSys_t* createSystem() {
 */
 
 flight_t* createFlight(airport_t* dest, timeHM_t dep, timeHM_t arr, int c) {
-   // Replace this line with your code
-   return NULL;
+	flight_t* flt = (flight_t*)malloc(sizeof(flight_t));	 
+   	flt->destination = dest;
+	flt->departure_time = dep;
+	flt->arrival_time = arr;
+	flt->cost = c;
+	flt->next = NULL;
+	return flt;
 }
 
 /*
@@ -55,6 +69,19 @@ flight_t* createFlight(airport_t* dest, timeHM_t dep, timeHM_t arr, int c) {
  */
 void deleteSystem(flightSys_t* s) {
     // Replace this line with your code
+	airport_t* airps = s->airports;
+	while(airps != NULL){
+		flight_t* flt = airps->flights;
+		while(flt != NULL){
+			flight_t* temp_flt = flt;
+			flt = flt->next;
+			free(temp);
+		}
+		airport* temp_airp = airps;
+		airps = airps->next;
+		free(temp_airp);
+	}
+	free(s);
 }
 
 
@@ -64,6 +91,28 @@ void deleteSystem(flightSys_t* s) {
  */
 void addAirport(flightSys_t* s, char* name) {
     // Replace this line with your code
+	airport_t *air = (airport_t*)malloc(sizeof(airport_t));
+	
+	if(air == NULL){
+		allocation_failed();	
+	}
+	air->flights = NULL;
+	air->name = (char*)calloc(strlen(name)+1,sizeof(char));
+	strcpy(air->name,name);
+	air->next = NULL;
+
+	//If the current airport list is empty, add it directly
+	if(s->airports == NULL){
+		s->airports = air;
+	}else{
+		//Append the airport to the rear of the list
+		airport_t *cur = s->airports
+		while(cur->next !=NULL){
+			cur = cur->next;
+		}
+		cur->next = air;
+	}
+	
 }
 
 
@@ -72,8 +121,12 @@ void addAirport(flightSys_t* s, char* name) {
    If the airport doesn't exist, return NULL.
  */
 airport_t* getAirport(flightSys_t* s, char* name) {
-    // Replace this line with your code
-    return NULL;
+	for(airport_t *cur = s->airports; cur!=NULL; cur = cur->next){
+		if(strcmp(name,cur->name) == 0){
+			return cur;
+		}
+	}
+	return NULL;
 }
 
 
@@ -82,8 +135,14 @@ airport_t* getAirport(flightSys_t* s, char* name) {
    Make sure to end with a new line. You should compare your output with the correct output
    in flights.out to make sure your formatting is correct.
  */
-void printAirports(flightSys_t* s) {
-    // Replace this line with your code
+	if(s->aiports == NULL){
+		
+	}else{
+		for(aiport_t *cur = s->airports; cur!=NULL;cur = cur->next){
+			printf(cur->name);
+			printf('\n');
+		}
+	}
 }
 
 
@@ -91,7 +150,20 @@ void printAirports(flightSys_t* s) {
    Adds a flight to src's schedule, stating a flight will leave to dst at departure time and arrive at arrival time.
  */
 void addFlight(airport_t* src, airport_t* dst, timeHM_t* departure, timeHM_t* arrival, int cost) {
-    // Replace this line with your code
+	if(!(src && dst && departure && arrival)){
+		printf(stderr,"Invalid parameter!");
+		exit(EXIT_FAILURE);
+	}
+	
+	//New flight to be added
+	flight_t *newFlt = createFlight(dst,*departure,*arrival,cost);
+	if(src->flights == NULL){
+		src->flights = newFlt;
+	}else{
+		flight_t *cur = src->flight;
+		for(;cur->next != NULL;cur = cur->next);
+		cur->next = newFlt;
+	}		
 }
 
 
@@ -114,6 +186,219 @@ void printSchedule(airport_t* s) {
    Given a src and dst airport, and the time now, finds the next flight to take based on the following rules:
    1) Finds the cheapest flight from src to dst that departs after now.
    2) If there are multiple cheapest flights, take the one that arrives the earliest.
+:w
+
+   If a flight is found, you should store the flight's departure time, arrival time, and cost in departure, arrival, 
+   and cost params and return true. Otherwise, return false. 
+
+   Please use the function isAfter() from time.h when comparing two timeHM_t objects.
+ */
+bool getNextFlight(airport_t* src, airport_t* dst, timeHM_t* now, timeHM_t* departure, timeHM_t* arrival, int* cost) {
+    // Replace this line with your code
+    return false;
+}
+
+/* Given a list of flight_t pointers (flight_list) and a list of destination airport names (airport_name_list), first confirm that it is indeed possible to take these sequences of flights,
+   (i.e. be sure that the i+1th flight departs after or at the same time as the ith flight arrives) (HINT: use the isAfter and isEqual functions).
+   Then confirm that the list of destination airport names match the actual destination airport names of the provided flight_t struct's.
+   sz tells you the number of flights and destination airport names to consider. Be sure to extensively test for errors (i.e. if you encounter NULL's for any values that you might expect to
+   be non-NULL, return -1).
+
+   Return from this function the total cost of taking these sequence of flights. If it is impossible to take these sequence of flights, if the list of destination airport names
+   doesn't match the actual destination airport names provided in the flight_t struct's, or if you run into any errors mentioned previously or any other errors, return -1.
+*/
+int validateFlightPath(flight_t** flight_list, char** airport_name_list, int sz) {
+    // Replace this line with your code
+    return -1;
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "flights.h"
+#include "timeHM.h"
+
+struct flightSys {
+    // Place the members you think are necessary for the flightSys struct here.
+	airport_t* airports;
+};
+
+struct airport {
+    // Place the members you think are necessary for the airport struct here.
+	char* name;
+	flight_t* flights;
+	airport* next; 
+};
+
+struct flight {
+    // Place the members you think are necessary for the flight struct here.
+	airport_t *destination;
+	time_HM departure_time;
+	time_HM arrival_time;
+	int cost;
+	flight* next;
+};
+
+/*
+   This should be called if memory allocation failed.
+ */
+static void allocation_failed() {
+    fprintf(stderr, "Out of memory.\n");
+    exit(EXIT_FAILURE);
+}
+
+
+/*
+   Creates and initializes a flight system, which stores the flight schedules of several airports.
+   Returns a pointer to the system created.
+ */
+flightSys_t* createSystem() {
+    //Create an empty system at first time
+
+	flightSys_t* s = (flightSys_t*)malloc(sizeof(flightSys_t));
+	if(s == NULL){
+		allocation_failed();
+	}
+	s->airports = NULL;
+	return s;
+}
+
+
+/* Given a destination airport, a departure and arrival time, and a cost, return a pointer to new flight_t. Note that this pointer must be available to use even
+   after this function returns. (What does this mean in terms of how this pointer should be instantiated)?
+*/
+
+flight_t* createFlight(airport_t* dest, timeHM_t dep, timeHM_t arr, int c) {
+	flight_t* flt = (flight_t*)malloc(sizeof(flight_t));	 
+   	flt->destination = dest;
+	flt->departure_time = dep;
+	flt->arrival_time = arr;
+	flt->cost = c;
+	flt->next = NULL;
+	return flt;
+}
+
+/*
+   Frees all memory associated with this system; that's all memory you dynamically allocated in your code.
+ */
+void deleteSystem(flightSys_t* s) {
+    // Replace this line with your code
+	airport_t* airps = s->airports;
+	while(airps != NULL){
+		flight_t* flt = airps->flights;
+		while(flt != NULL){
+			flight_t* temp_flt = flt;
+			flt = flt->next;
+			free(temp);
+		}
+		airport* temp_airp = airps;
+		airps = airps->next;
+		free(temp_airp);
+	}
+	free(s);
+}
+
+
+/*
+   Adds a airport with the given name to the system. You must copy the string and store it.
+   Do not store "name" (the pointer) as the contents it point to may change.
+ */
+void addAirport(flightSys_t* s, char* name) {
+    // Replace this line with your code
+	airport_t *air = (airport_t*)malloc(sizeof(airport_t));
+	
+	if(air == NULL){
+		allocation_failed();	
+	}
+	air->flights = NULL;
+	air->name = (char*)calloc(strlen(name)+1,sizeof(char));
+	strcpy(air->name,name);
+	air->next = NULL;
+
+	//If the current airport list is empty, add it directly
+	if(s->airports == NULL){
+		s->airports = air;
+	}else{
+		//Append the airport to the rear of the list
+		airport_t *cur = s->airports
+		while(cur->next !=NULL){
+			cur = cur->next;
+		}
+		cur->next = air;
+	}
+	
+}
+
+
+/*
+   Returns a pointer to the airport with the given name.
+   If the airport doesn't exist, return NULL.
+ */
+airport_t* getAirport(flightSys_t* s, char* name) {
+	for(airport_t *cur = s->airports; cur!=NULL; cur = cur->next){
+		if(strcmp(name,cur->name) == 0){
+			return cur;
+		}
+	}
+	return NULL;
+}
+
+
+/*
+   Print each airport name in the order they were added through addAirport, one on each line.
+   Make sure to end with a new line. You should compare your output with the correct output
+   in flights.out to make sure your formatting is correct.
+ */
+	if(s->aiports == NULL){
+		
+	}else{
+		for(aiport_t *cur = s->airports; cur!=NULL;cur = cur->next){
+			printf(cur->name);
+			printf('\n');
+		}
+	}
+}
+
+
+/*
+   Adds a flight to src's schedule, stating a flight will leave to dst at departure time and arrive at arrival time.
+ */
+void addFlight(airport_t* src, airport_t* dst, timeHM_t* departure, timeHM_t* arrival, int cost) {
+	if(!(src && dst && departure && arrival)){
+		printf(stderr,"Invalid parameter!");
+		exit(EXIT_FAILURE);
+	}
+	
+	//New flight to be added
+	flight_t *newFlt = createFlight(dst,*departure,*arrival,cost);
+	if(src->flights == NULL){
+		src->flights = newFlt;
+	}else{
+		flight_t *cur = src->flight;
+		for(;cur->next != NULL;cur = cur->next);
+		cur->next = newFlt;
+	}		
+}
+
+
+/*
+   Prints the schedule of flights of the given airport.
+
+   Prints the airport name on the first line, then prints a schedule entry on each 
+   line that follows, with the format: "destination_name departure_time arrival_time $cost_of_flight".
+
+   You should use printTime (look in timeHM.h) to print times, and the order should be the same as 
+   the order they were added in through addFlight. Make sure to end with a new line.
+   You should compare your output with the correct output in flights.out to make sure your formatting is correct.
+ */
+void printSchedule(airport_t* s) {
+    // Replace this line with your code
+}
+
+
+/*
+   Given a src and dst airport, and the time now, finds the next flight to take based on the following rules:
+   1) Finds the cheapest flight from src to dst that departs after now.
+   2) If there are multiple cheapest flights, take the one that arrives the earliest.
+:w
 
    If a flight is found, you should store the flight's departure time, arrival time, and cost in departure, arrival, 
    and cost params and return true. Otherwise, return false. 
